e65828f361263ccacf693720cb36400c7f83b511
Stanislav Pimenov <stanislav_pimenov@epam.com>
2018-01-15 22:50:01 +0300

    code review changes

--- microservice-2-feign/src/main/java/com/epam/microservice/hello/consumer/Application.java
+++ microservice-2-feign/src/main/java/com/epam/microservice/hello/consumer/Application.java
@@ -1,15 +1,11 @@
 package com.epam.microservice.hello.consumer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 public class Application {
